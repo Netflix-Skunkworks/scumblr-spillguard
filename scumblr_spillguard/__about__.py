645de0276ca5fa5208db30377414cafbae6e0ffa
Kevin Glisson <kevgliss@gmail.com>
2018-01-04 15:14:39 -0800

    Fixing packaging issues.

---
+++ scumblr_spillguard/__about__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, division, print_function
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "scumblr-spillguard"
+__summary__ = ("Secret monitoring of github commits.")
+__uri__ = "https://github.com/Netflix-Skunkworks/scumblr-spillguard"
+
+__version__ = "0.1.0"
+
+__author__ = "The Scumblr developers"
+__email__ = "security@netflix.com"
+
+__license__ = "Apache License, Version 2.0"
+__copyright__ = "Copyright 2018 {0}".format(__author__)
---
+++ setup.py
@@ -0,0 +1,54 @@
+"""
+Scumblr-Spillguard
+==================
+
+Processes github commit events to look for known secrets.
+"""
+import sys
+import os.path
+
+from setuptools import setup, find_packages
+
+
+ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__)))
+
+# When executing the setup.py, we need to be able to import ourselves, this
+# means that we need to add the src/ directory to the sys.path.
+sys.path.insert(0, ROOT)
+
+about = {}
+with open(os.path.join(ROOT, "scumbler_spillguard", "__about__.py")) as f:
+    exec(f.read(), about)
+
+
+install_requires = [
+    'requests==2.11.1',
+    'retrying',
+    'raven_python_lambda'
+]
+
+tests_require = []
+
+
+setup(
+    name=about["__title__"],
+    version=about["__version__"],
+    author=about["__author__"],
+    author_email=about["__email__"],
+    url=about["__uri__"],
+    description=about["__summary__"],
+    long_description='See README.md',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'tests': tests_require
+    },
+    keywords=['github', 'secret_management'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
