424b069bd1164f071987b2d1cae56681e7040be8
Kevin Glisson <kevgliss@gmail.com>
2018-01-08 15:17:25 -0800

    Adding missing variable name.

--- scumblr_spillguard/secrets.py
+++ scumblr_spillguard/secrets.py
@@ -5,6 +5,6 @@ from scumblr_spillguard import log
 
 def get_secret(name):
     """Retrieves secret from KMS using the name env variable."""
-    log.info('Fetching secret from env var. VAR: {}')
+    log.info('Fetching secret from env var. VAR: {}'.format(name))
     kms = boto3.session.Session().client("kms")
     return kms.decrypt(CiphertextBlob=os.environ[name].encode('utf-8'))["Plaintext"]
