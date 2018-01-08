5aeafaa42a751ec669007eddba3aea585d13e0cd
Kevin Glisson <kevgliss@gmail.com>
2018-01-08 15:51:19 -0800

    Adding b64 decode.

--- scumblr_spillguard/secrets.py
+++ scumblr_spillguard/secrets.py
@@ -1,4 +1,5 @@
 import os
+import base64
 import boto3
 from scumblr_spillguard import log
 
@@ -7,4 +8,4 @@ def get_secret(name):
     """Retrieves secret from KMS using the name env variable."""
     log.info('Fetching secret from env var. VAR: {}'.format(name))
     kms = boto3.session.Session().client("kms")
-    return kms.decrypt(CiphertextBlob=os.environ[name].encode('utf-8'))["Plaintext"]
+    return kms.decrypt(CiphertextBlob=base64.b64decode(os.environ[name]))["Plaintext"]
