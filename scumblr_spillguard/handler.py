9f301f7b199bf24f0440a326b06e80f8ad9f260f
Kevin Glisson <kevgliss@gmail.com>
2018-01-10 09:15:22 -0800

    No source for github events.

--- scumblr_spillguard/handler.py
+++ scumblr_spillguard/handler.py
@@ -80,7 +80,7 @@ def github_handler(event, context):
 
     # github has a very low timeout (10s) we make sure that we can prewarm our function to prevent
     # the service from timing out
-    if event['source'] == 'aws.events':
+    if event.get('source') == 'aws.events':
         return {'statusCode': '200', 'body': '{}'}
 
     github.validate(event)
